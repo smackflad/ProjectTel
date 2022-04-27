06c022b4fb74756e3a751a6d810244311a99430a
smackflad <ask.kalamakhs@hotmail.com>
2022-04-27 19:24:07 +0300

    fixing textarea

--- provider-app/src/components/generalComponents/MyTextArea/MyTextArea.js
+++ provider-app/src/components/generalComponents/MyTextArea/MyTextArea.js
@@ -1,16 +1,16 @@
 import { useState } from 'react';
 import './MyTextArea.css';
 
-const MyTextArea = ({txt, filled, setFilled}) => 
+const MyTextArea = ({labelTxt, val, setVal=(a)=>{}, star=false, disabled=false, rows="100", cols="50"}) => 
 {
     const [id] = useState(() => `component-${Math.random().toString(16).slice(2)}`);
     return (
 		<div className="txt-area-external">
             <div className='txt-area-internal'>
                 <div className="txt-area-top">
-                    <span><span id="star">*</span><label for={id}>{txt}</label></span>
+                    <span>{star && (<span id="star">*</span>)}<label htmlFor={id}>{labelTxt}</label></span>
                 </div>
-                <textarea id={id} onChange={(e)=>{setFilled(e.target.value)}} defaultValue={filled}></textarea>
+                <textarea id={id} onChange={(e)=>{setVal(e.target.value)}} defaultValue={val} rows={rows} cols={cols} disabled={disabled}></textarea>
             </div>
         </div>
 	);
