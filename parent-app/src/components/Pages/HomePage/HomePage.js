873a6372c0d124561246530953002a79f4a08425
smackflad <ask.kalamakhs@hotmail.com>
2022-05-09 12:59:01 +0300

    moved files to correct folders

--- parent-app/src/components/Pages/HomePage/HomePage.js
+++ parent-app/src/components/Pages/HomePage/HomePage.js
@@ -1,8 +1,8 @@
 import "./myHomePage.css";
-import HomeTop from "./components/HomeTop/HomeTop";
-import HomeBot from "./components/HomeBot/HomeBot";
-
+import HomeTop from "./Components/HomeTop/HomeTop";
+import HomeBot from "./Components/HomeBot/HomeBot";
 import Middle_Component from "./Components/Middle_Component/Middle_Component";
+
 import { Navigate } from "react-router-dom";
 
 const HomePage = () => {	
