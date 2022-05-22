37644e6bbc86d7e3a639a824022c64ad94409228
yannispapadopoulos <56275978+yannispapadopoulos@users.noreply.github.com>
2022-05-22 14:32:27 +0300

    refactor

--- parent-app/src/components/Pages/parentProfile/ParentProfilePage.js
+++ parent-app/src/components/Pages/parentProfile/ParentProfilePage.js
@@ -2,103 +2,103 @@ import "./parentProfilePage.css";
 import { Navigate } from "react-router-dom";
 
 const ParentProfilePage = () => {
-	return (
-		<div className="ParentProfilePage -external">
+    return (
+        <div className="ParentProfilePage -external">
 
-<body>
-    <div class="container">
-        <header> User Profile</header>
+            <body>
+                <div class="container">
+                    <header> User Profile</header>
 
-                <div class="myForm">
+                    <div class="myForm">
 
-                    <div class="fields">
+                        <div class="fields">
 
-                        <div class="input-field">
-                            <label>First Name</label>
-                            <input type="text" placeholder="Enter first name" required></input>
-                        </div>
+                            <div class="input-field">
+                                <label>First Name</label>
+                                <input type="text" placeholder="Enter first name" required></input>
+                            </div>
 
-                        <div class="input-field">
-                            <label>Last Name</label>
-                            <input type="text" placeholder="Enter last name" required></input>
-                        </div>
+                            <div class="input-field">
+                                <label>Last Name</label>
+                                <input type="text" placeholder="Enter last name" required></input>
+                            </div>
 
-                        <div class="input-field">
-                            <label>Email</label>
-                            <input type="text" placeholder="myemail@mail.com" required></input>
-                        </div>
+                            <div class="input-field">
+                                <label>Email</label>
+                                <input type="text" placeholder="myemail@mail.com" required></input>
+                            </div>
 
 
-                        <div class="input-field">
-                            <label>Old Password</label>
-                            <input type="text" ></input>
-                        </div>
+                            <div class="input-field">
+                                <label>Old Password</label>
+                                <input type="text"></input>
+                            </div>
 
-                        <div class="input-field">
-                            <label>New Password</label>
-                            <input type="text" ></input>
-                        </div>
+                            <div class="input-field">
+                                <label>New Password</label>
+                                <input type="text"></input>
+                            </div>
 
-                        <button class="changeNameBtn">
+                            <button class="changeNameBtn">
                                 <span class="btnText">update information</span>
                                 <i class="uil uil-navigator"></i>
                             </button>
 
-                    </div>
-                    
-                </div>  
+                        </div>
 
-                             <div class="myForm2">  
-                           
-                             <div class="label-field">
+                    </div>
+
+                    <div class="myForm2">
+
+                        <div class="label-field">
                             <label>Current Balance: </label>
                             <label>$$</label>
                         </div>
-                        
+
                         <div class="label-field">
-                        <label>Choose amount: </label>
-                        
+                            <label>Choose amount: </label>
+
                         </div>
                         <button class="changeNameBtn">
-                                <span class="btnText">∙5 $</span>
-                                <i class="uil uil-navigator"></i>
-                            </button>
+                            <span class="btnText">∙5 $</span>
+                            <i class="uil uil-navigator"></i>
+                        </button>
 
-                            <button class="changeNameBtn">
-                                <span class="btnText">∙10 $</span>
-                                <i class="uil uil-navigator"></i>
-                            </button>
-                            <button class="changeNameBtn">
-                                <span class="btnText">∙50 $</span>
-                                <i class="uil uil-navigator"></i>
-                            </button>
+                        <button class="changeNameBtn">
+                            <span class="btnText">∙10 $</span>
+                            <i class="uil uil-navigator"></i>
+                        </button>
+                        <button class="changeNameBtn">
+                            <span class="btnText">∙50 $</span>
+                            <i class="uil uil-navigator"></i>
+                        </button>
 
-                      
+
                         <div class="label-field">
-                        <label>My cards: </label>
+                            <label>My cards: </label>
                         </div>
 
                         <button class="changeNameBtn">
-                                <span class="btnText">∙Card 1</span>
-                                <i class="uil uil-navigator"></i>
-                            </button>
-                            <button class="changeNameBtn">
-                                <span class="btnText">∙Card 2</span>
-                                <i class="uil uil-navigator"></i>
-                            </button>
+                            <span class="btnText">∙Card 1</span>
+                            <i class="uil uil-navigator"></i>
+                        </button>
+                        <button class="changeNameBtn">
+                            <span class="btnText">∙Card 2</span>
+                            <i class="uil uil-navigator"></i>
+                        </button>
 
-                            <button class="changeNameBtn">
-                                <span class="btnText">∙Add new card</span>
-                                <i class="uil uil-navigator"></i>
-                            </button>
-                             </div>    
+                        <button class="changeNameBtn">
+                            <span class="btnText">∙Add new card</span>
+                            <i class="uil uil-navigator"></i>
+                        </button>
+                    </div>
 
 
-    </div>
+                </div>
 
-</body>
-		</div>
-	);
+            </body>
+        </div>
+    );
 };
 
 export default ParentProfilePage;
