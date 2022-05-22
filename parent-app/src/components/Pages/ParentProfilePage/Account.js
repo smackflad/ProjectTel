d2351bf55098454280a6ea91211b299a8ddc0ae8
alexgougou13 <alex_gougoulis@hotmail.com>
2022-05-22 21:26:39 +0300

    Account done

--- parent-app/src/components/Pages/ParentProfilePage/Account.js
+++ parent-app/src/components/Pages/ParentProfilePage/Account.js
@@ -41,7 +41,48 @@ const Account = () => {
           disabled
         ></input>
       </div>
-      <div className="Account-password"></div>
+      <div className="Account-password">
+        <h4>Αλλαγή κωδικού</h4>
+        <form className="password-change">
+          <input
+            type="password"
+            id="Oldpassword"
+            name="Oldpassword"
+            required
+            // value={form.password}
+            // onChange={handleChange}
+            placeholder="Τωρινός κωδικός πρόσβασης"
+          ></input>
+          <input
+            type="password"
+            id="NewPassword"
+            name="NewPassword"
+            required
+            // value={form.password}
+            // onChange={handleChange}
+            placeholder="Νέος κωδικός πρόσβασης"
+          ></input>
+          <input
+            type="password"
+            id="NewPasswordVer"
+            name="NewPasswordVer"
+            required
+            // value={form.password}
+            // onChange={handleChange}
+            placeholder="Επανάληψη νέου κωδικού πρόσβασης"
+          ></input>
+          <button
+            type="submit"
+            style={{
+              backgroundColor: "#1AABBF",
+              color: "#ffffff",
+              fontSize: "16px",
+            }}
+          >
+            Αλλαγή κωδικού
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
