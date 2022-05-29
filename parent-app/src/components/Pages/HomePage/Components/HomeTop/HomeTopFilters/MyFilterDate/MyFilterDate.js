82da4d589a0bdfcac496338dbd138d935bde3da3
smackflad <ask.kalamakhs@hotmail.com>
2022-05-29 21:14:11 +0300

    filter date possibly done, need to make it responsive

--- parent-app/src/components/Pages/HomePage/Components/HomeTop/HomeTopFilters/MyFilterDate/MyFilterDate.js
+++ parent-app/src/components/Pages/HomePage/Components/HomeTop/HomeTopFilters/MyFilterDate/MyFilterDate.js
@@ -4,48 +4,57 @@ import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import DatePicker from "react-datepicker";
 
-const DateCategories = [{item: "Σήμερα", value:0},{item: "Αυτή την Εβδομάδα", value:1},{item: "Συγκεκριμένο διάστημα", value:2}];
+const DateCategories = [{item: "Οποτεδήποτε", value:0}, {item: "Σήμερα", value:1},{item: "Αυτή την Εβδομάδα", value:2}];
 
 const MyFilterDate = ({}) => 
 {
-	const [startDate, setStartDate] = useState(new Date());
-	const [endDate, setEndDate] = useState(null);
+	const [startDate, setStartDate] = useState();
+	const [endDate, setEndDate] = useState();
     
-    const [curr, setCurr] = useState(-5);
+    const [curr, setCurr] = useState(0);
 
-	const [open, setOpen] = useState(true);
+	const [open, setOpen] = useState(false);
 
     const onChange = (dates) => {
         const [start, end] = dates;
         setStartDate(start);
         setEndDate(end);
-        setCurr(2);
+        if(end != null){
+            setCurr(3);
+        }else{
+            setCurr(0);
+        }
     };
 
     const handleCheck = (event) => {
         if (event.target.checked) {
             setCurr(event.target.value);
+            // setStartDate(null);
+            setEndDate(null);
+            setStartDate(null);
         } else {
-            setCurr(-5);
+            if(endDate != null)endDate.setDate(null);
+            setCurr(0);
         }
-        // console.log(curr);
     };
 
     var checkedItems = ()=>{
-        if(curr == 0){
+        if(curr == 1){
             const currDate = new Date();
             return currDate.getDate() + '/' + currDate.getMonth();
-        }else if(curr == 1){
+        }else if(curr == 2){
             var today = new Date();
             var lastday = new Date();
             while(lastday.getDay() != 0){
                 lastday.setDate(lastday.getDate()+1);
             }
             return today.getDate() + '/' + today.getMonth() + ' - ' + lastday.getDate() + '/' + lastday.getMonth();
-        }else if(curr == 2){
-        
+        }else if(curr == 3){
+            if(startDate && endDate)
             return startDate.getDate() + '/' + startDate.getMonth() + ' - ' + endDate.getDate() + '/' + endDate.getMonth();
+        }else if(curr == 0){
         }
+        return '';
     }
 
     return (
@@ -53,8 +62,8 @@ const MyFilterDate = ({}) =>
         <div className="MyFilter-txt-external MyFilterDate-txt-external">
             <div onClick={()=>{setOpen(!open)}} className='MyFilter-txt-internal MyFilterDate-txt-internal'>
                 <div className={`MyFilter-txt-internal-spans`}>
-                    <span className={`MyFilter-txt-span ${curr !== -5 ? "MyFilter-txt-span-small":""}`}>Ημερομηνία</span>
-                    {curr !== -5 &&
+                    <span className={`MyFilter-txt-span ${curr != 0 ? "MyFilter-txt-span-small":""}`}>Ημερομηνία</span>
+                    {curr != 0 &&
                             <span className='MyFilter-txt-span-items'>{checkedItems()}</span>
                     }
                 </div>
@@ -69,13 +78,14 @@ const MyFilterDate = ({}) =>
                         <div className='MyFilterDate-popup-internal-items'>
                             <div className="MyFilterDate-popup-internal-map">
                                 <DatePicker
-                                    selected={startDate}
+                                    selected={null}
                                     onChange={onChange}
-                                    monthsShown={2}
                                     startDate={startDate}
                                     endDate={endDate}
-                                    selectsRange
+                                    selectsRange={true}
+                                    isClearable={true}
                                     inline
+                                    monthsShown={2}
                                 />
                                 <ul>
                                     {
