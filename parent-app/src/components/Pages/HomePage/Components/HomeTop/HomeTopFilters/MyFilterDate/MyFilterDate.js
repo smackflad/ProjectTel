f33d3c0dd536a0cf49afa176a766e8746a27c0e0
smackflad <ask.kalamakhs@hotmail.com>
2022-05-31 20:55:04 +0300

    made responsive filters

--- parent-app/src/components/Pages/HomePage/Components/HomeTop/HomeTopFilters/MyFilterDate/MyFilterDate.js
+++ parent-app/src/components/Pages/HomePage/Components/HomeTop/HomeTopFilters/MyFilterDate/MyFilterDate.js
@@ -86,7 +86,68 @@ const MyFilterDate = ({}) =>
                                     isClearable={true}
                                     inline
                                     monthsShown={2}
+                                    popperClassName="test"
                                 />
+
+                                {/* <DatePicker
+                                    renderCustomHeader={({
+                                        monthDate,
+                                        customHeaderCount,
+                                        decreaseMonth,
+                                        increaseMonth,
+                                        }) => (
+                                            <div>
+                                            <button
+                                                aria-label="Previous Month"
+                                                className={
+                                                "react-datepicker__navigation react-datepicker__navigation--previous"
+                                                }
+                                                style={customHeaderCount === 1 ? { visibility: "hidden" } : null}
+                                                onClick={decreaseMonth}
+                                            >
+                                                <span
+                                                className={
+                                                    "react-datepicker__navigation-icon react-datepicker__navigation-icon--previous"
+                                                }
+                                                >
+                                                {"<"}
+                                                </span>
+                                            </button>
+                                            <span className="react-datepicker__current-month">
+                                                {monthDate.toLocaleString("en-US", {
+                                                month: "long",
+                                                year: "numeric",
+                                                })}
+                                            </span>
+                                            <button
+                                                aria-label="Next Month"
+                                                className={
+                                                "react-datepicker__navigation react-datepicker__navigation--next"
+                                                }
+                                                style={customHeaderCount === 0 ? { visibility: "hidden" } : null}
+                                                onClick={increaseMonth}
+                                            >
+                                                <span
+                                                className={
+                                                    "react-datepicker__navigation-icon react-datepicker__navigation-icon--next"
+                                                }
+                                                >
+                                                {">"}
+                                                </span>
+                                            </button>
+                                            </div>
+                                    )}
+                                    onChange={(date) => setStartDate(date)}
+                                    monthsShown={2}
+                                    inline
+                                    selected={null}
+                                    startDate={startDate}
+                                    endDate={endDate}
+                                    selectsRange={true}
+                                    isClearable={true}
+                                /> */}
+
+
                                 <ul>
                                     {
                                         DateCategories.slice(0,DateCategories.length).map((item, index)=>
