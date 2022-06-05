2fdbc111ddd471c5d5343268ac60c81e9255c12a
smackflad <ask.kalamakhs@hotmail.com>
2022-06-05 14:34:05 +0300

    made middle component work with mock data(need to setup location)

--- parent-app/src/components/Pages/HomePage/Components/Event/Event.js
+++ parent-app/src/components/Pages/HomePage/Components/Event/Event.js
@@ -1,17 +1,24 @@
 import "./Event.css";
 import { useState } from "react";
 
-const Event = ({ title, venue }) => {
+var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+"Jul", "Aug", "Sept", "Oct", "Nov", "Dec" ];
+
+const Event = ({ id, title, venue, img, date }) => {
+  const datee = new Date(date);
+  // console.log(datee.getDate()+" "+monthNames[datee.getMonth()]);
+  // {datee.getDate()+" "+monthNames[datee.getMonth()]}
+  console.log(id);
   return (
     <div className="Inside_Middle_Component-external">
-      <a className="Inside_Middle_Component-elementClick" href="/event">
+      <a className="Inside_Middle_Component-elementClick" href={"/event/"+id}>
         <img
           className="Inside-Middle-Component-image"
-          src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+          src={img}
           alt=""
         ></img>
         <div className="Inside_Middle_Component-info">
-          <span className="Inside_Middle_Component-date">25 MAY</span>
+          <span className="Inside_Middle_Component-date">{datee.getDate()+" "+monthNames[datee.getMonth()]}</span>
           <span className="Inside_Middle_Component-title">{title}</span>
           <span className="Inside_Middle_Component-venue">{venue}</span>
         </div>
