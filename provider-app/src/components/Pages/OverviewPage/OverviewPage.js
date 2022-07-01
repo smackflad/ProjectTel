dadffc7e72860b1f15e9606ee2cff8fbe98375d4
smackflad <ask.kalamakhs@hotmail.com>
2022-07-02 02:56:12 +0300

    started doing important overview stuff

--- provider-app/src/components/Pages/OverviewPage/OverviewPage.js
+++ provider-app/src/components/Pages/OverviewPage/OverviewPage.js
@@ -5,6 +5,10 @@ import MyTextArea from "../../generalComponents/MyTextArea/MyTextArea";
 import { useState } from "react";
 import MyButton from "../../generalComponents/MyButton/MyButton";
 import MySelectBox from "../../generalComponents/MySelectBox/MySelectBox";
+import { VictoryLine, VictoryChart, VictoryAxis, VictoryScatter, VictoryGroup } from 'victory';
+
+
+
 
 const OverviewPage = () => {
 	const [file_name, setFile_name] = useState("test");
@@ -12,6 +16,12 @@ const OverviewPage = () => {
 	const [on_change_f, setOn_change_f] = useState();
 	const [disabled, setDisabled] = useState();
 
+	const data = [
+		{quarter: 1, earnings: 13000},
+		{quarter: 2, earnings: 16500},
+		{quarter: 3, earnings: 14250},
+		{quarter: 4, earnings: 19000}
+	  ];
 
 	return (
 		<div className="OverviewPage-external">
@@ -47,7 +57,41 @@ const OverviewPage = () => {
 						<span>Event Statistics</span>
 						<div className="OverviewPage-bot-inner">
 							<div className="OverviewPage-bot-left">
-								
+							<VictoryChart
+								// domainPadding will add space to each side of VictoryBar to
+								// prevent it from overlapping the axis
+								// domainPadding={20}
+							>
+								<VictoryAxis
+									// tickValues specifies both the number of ticks and where
+									// they are placed on the axis
+									tickValues={[1, 2, 3, 4, 5]}
+									tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4", "Quarter 5"]}
+								/>
+								<VictoryAxis
+									dependentAxis
+									// tickFormat specifies how ticks should be displayed
+									tickFormat={(x) => (`$${x / 1}k`)}
+								/>
+								<VictoryGroup
+									data={[
+									{ x: 1, y: 2 },
+									{ x: 2, y: 3 },
+									{ x: 3, y: 5 },
+									{ x: 4, y: 4 },
+									{ x: 5, y: 7 }
+									]}
+								>
+
+								<VictoryLine
+									style={{
+									data: { stroke: "#c43a31" },
+									parent: { border: "1px solid #ccc"}
+									}}
+								/>
+								<VictoryScatter size={4}/>
+								</VictoryGroup>
+							</VictoryChart>
 							</div>
 							<div className="OverviewPage-bot-right">
 								
