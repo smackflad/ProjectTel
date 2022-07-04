0befdcb06fb1f792510c642cafa57e35c5a46157
yannispapadopoulos <56275978+yannispapadopoulos@users.noreply.github.com>
2022-07-04 17:04:36 +0300

    usercreationpage

---
+++ provider-app/src/components/Pages/UserCreationPage/UserCreationPage.js
@@ -0,0 +1,103 @@
+import "./userCreationPage.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+
+const userCreationPage = () => {
+  // // console.log("ok");      
+  // const { profile, loading } = useSelector((state) => state.userAccount);
+  // const dispatch = useDispatch();
+  // useEffect(() => {
+  //   (async () => {
+  //     try {
+  //       const res = await dispatch(fetchUserAccount(2)).unwrap();
+  //       console.log(`success user id: ${res.data.id}`);
+  //     } catch (err) {
+  //       console.error(err.message);
+  //     }
+  //     // console.log("hello");
+  //   })();
+  // }, [dispatch]);
+  // console.log(loading);
+  // console.log(profile);
+  return (
+    <div className="userCreationPage-external">
+      <div className="container-selection">
+
+        <h1>Εισαγωγή Νέου Χρήστη</h1>
+       
+        <input
+          className="userCreationPage-inputs"
+          type="text"
+          placeholder="Όνομα"
+          id="firstName"
+         // value={form.firstName}
+          //   onChange={handleChange}
+          required
+        ></input>
+        <input
+          className="userCreationPage-inputs"
+          type="text"
+          placeholder="Επίθετο"
+          id="lastName"
+         // value={form.lastName}
+          //   onChange={handleChange}
+          required
+        ></input>
+
+
+        <input className="userCreationPage-inputs"
+          type="text"
+          id="Role"
+          name="Role"
+          placeholder="Ρόλος"
+          required
+          list="roles" 
+          //value={form.Role}
+          />
+        <datalist id="roles">
+          <option value="companyAdmin">Χρήστης Διαχείρισης</option>
+          <option value="companyupport">Χρήστης Υποστήριξης</option>
+        </datalist>
+
+
+        <input
+          className="userCreationPage-inputs"
+          type="text"
+          id="Password"
+          name="Password"
+          required
+          //value={form.Password}
+          // onChange={handleChange}
+          placeholder="Κωδικός πρόσβασης"
+        ></input>
+        <input
+          className="userCreationPage-inputs"
+          type="text"
+          id="PasswordVer"
+          name="PasswordVer"
+          required
+          //value={form.PasswordVer}
+          // onChange={handleChange}
+          placeholder="Επανάληψη κωδικού πρόσβασης"
+        ></input>
+        <button
+          className="userCreationPage-inputs"
+          type="submit"
+          style={{
+            backgroundColor: "#1AABBF",
+            color: "#ffffff",
+            fontSize: "16px",
+          }}
+        >
+          Δημιουργία Χρήστη
+        </button>
+
+
+      </div>
+    </div>
+
+
+  );
+};
+
+export default ProviderProfilePage;
