0298aba72cbe8842e7f114d2f869c7893dddfaae
kostas antzou <kostas.best@gmail.com>
2022-07-07 23:50:23 +0300

    about us - provider

--- provider-app/src/components/Pages/AboutUs/AboutUs.js
+++ provider-app/src/components/Pages/AboutUs/AboutUs.js
@@ -1,14 +1,33 @@
 import "./AboutUs.css";
-import Monkey from "../../../images/404img.png";
 import { useNavigate } from "react-router-dom";
+import kidsPic from '../../../images/kids.jpg';
+import internetPic from '../../../images/internet.jpg';
 
 const AboutUs = () => {
   return (
     <div className="AboutUsPage-external">      
           <span className="AboutUsPage-title"> Σχετικά με εμάς </span>
 
-          <span className="AboutUsPage-text"> Είμαστε μια ομάδα φοιτητών της σχολής Πληροφορικής Και Τηλεπικοινωνιών που αποφασίσαμε να κάνουμε την επιλογή 
+          <span className="AboutUsPage-paragraph">- Ποιοί είμαστε? </span>
+
+          <span className="AboutUsPage-text"> Είμαστε μια ομάδα πέντε φοιτητών της σχολής Πληροφορικής Και Τηλεπικοινωνιών που αποφασίσαμε να κάνουμε την επιλογή 
           δραστηριοτήτων για τα παιδιά πιο εύκολη και γρήγορη απο ποτέ!  </span>
+
+          <img classname="AboutUsPage-image" src={kidsPic} alt=""></img>
+
+          <span className="AboutUsPage-paragraph">- Τί κάνουμε? </span>
+
+          <span className="AboutUsPage-text"> Προσφέρουμε μια πλατφόρμα για εύκολη εξυπηρέτηση των εταιριών που παρέχουν δραστηριότητες για παιδιά και νέους με 
+          πλήρεις δυνατότητες αγοραπωλησίας εικoνικών εισητηρίων.</span>
+
+          <img classname="AboutUsPage-image" src={internetPic} alt=""></img>
+
+          <span className="AboutUsPage-paragraph">- Γιατί να χρησιμοποιήσετε τις υπηρεσίες μας? </span>
+
+          <span className="AboutUsPage-text"> Γιατί με τις προηγμένες τεχνικές marketing, παίρνουμε το πιο ανταγωνιστικό ποσοστό απο τις αγορές των
+          εισητηρίων σας, μεγιστοποιόντας τα δικά σας κέρδη! </span>
+
+
     </div>
   );
 };
