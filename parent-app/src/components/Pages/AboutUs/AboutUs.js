26d7ab3baddb2d3e7dc7e2eed562381deaac48f3
alexgougou13 <alex_gougoulis@hotmail.com>
2022-07-08 02:18:31 +0300

    orderHistory

--- parent-app/src/components/Pages/AboutUs/AboutUs.js
+++ parent-app/src/components/Pages/AboutUs/AboutUs.js
@@ -1,28 +1,34 @@
 import "./AboutUs.css";
 import { useNavigate } from "react-router-dom";
-import kidsPic from '../../../images/kids.jpg';
-import internetPic from '../../../images/internet.jpg';
+import kidsPic from "../../../images/kids.jpg";
+import internetPic from "../../../images/internet.jpg";
 
 const AboutUs = () => {
   return (
-    <div className="AboutUsPage-external">      
-          <span className="AboutUsPage-title"> Σχετικά με εμάς </span>
+    <div className="AboutUsPage-external">
+      <span className="AboutUsPage-title"> Σχετικά με εμάς </span>
 
-          <span className="AboutUsPage-paragraph">- Ποιοί είμαστε? </span>
+      <span className="AboutUsPage-paragraph">- Ποιοί είμαστε? </span>
 
-          <span className="AboutUsPage-text"> Είμαστε μια ομάδα πέντε φοιτητών της σχολής Πληροφορικής Και Τηλεπικοινωνιών που αποφασίσαμε να κάνουμε την επιλογή 
-          δραστηριοτήτων για τα παιδιά πιο εύκολη και γρήγορη απο ποτέ!  </span>
+      <span className="AboutUsPage-text">
+        {" "}
+        Είμαστε μια ομάδα πέντε φοιτητών της σχολής Πληροφορικής Και
+        Τηλεπικοινωνιών που αποφασίσαμε να κάνουμε την επιλογή δραστηριοτήτων
+        για τα παιδιά πιο εύκολη και γρήγορη απο ποτέ!{" "}
+      </span>
 
-          <img classname="AboutUsPage-image" src={kidsPic} alt=""></img>
+      <img className="AboutUsPage-image" src={kidsPic} alt=""></img>
 
-          <span className="AboutUsPage-paragraph">- Τί κάνουμε? </span>
+      <span className="AboutUsPage-paragraph">- Τί κάνουμε? </span>
 
-          <span className="AboutUsPage-text"> Προσφέρουμε μια πλατφόρμα για εύκολη εξυπηρέτηση όσον που ψάχνουν δραστηριότητες για παιδιά και νέους με 
-          πλήρεις δυνατότητες αγοραπωλησίας εικoνικών εισητηρίων.</span>
+      <span className="AboutUsPage-text">
+        {" "}
+        Προσφέρουμε μια πλατφόρμα για εύκολη εξυπηρέτηση όσον που ψάχνουν
+        δραστηριότητες για παιδιά και νέους με πλήρεις δυνατότητες αγοραπωλησίας
+        εικoνικών εισητηρίων.
+      </span>
 
-          <img classname="AboutUsPage-image" src={internetPic} alt=""></img>
-
-
+      <img className="AboutUsPage-image" src={internetPic} alt=""></img>
     </div>
   );
 };
--- provider-app/src/components/Pages/AboutUs/AboutUs.js
+++ provider-app/src/components/Pages/AboutUs/AboutUs.js
@@ -1,33 +1,45 @@
 import "./AboutUs.css";
 import { useNavigate } from "react-router-dom";
-import kidsPic from '../../../images/kids.jpg';
-import internetPic from '../../../images/internet.jpg';
+import kidsPic from "../../../images/kids.jpg";
+import internetPic from "../../../images/internet.jpg";
 
 const AboutUs = () => {
   return (
-    <div className="AboutUsPage-external">      
-          <span className="AboutUsPage-title"> Σχετικά με εμάς </span>
+    <div className="AboutUsPage-external">
+      <span className="AboutUsPage-title"> Σχετικά με εμάς </span>
 
-          <span className="AboutUsPage-paragraph">- Ποιοί είμαστε? </span>
+      <span className="AboutUsPage-paragraph">- Ποιοί είμαστε? </span>
 
-          <span className="AboutUsPage-text"> Είμαστε μια ομάδα πέντε φοιτητών της σχολής Πληροφορικής Και Τηλεπικοινωνιών που αποφασίσαμε να κάνουμε την επιλογή 
-          δραστηριοτήτων για τα παιδιά πιο εύκολη και γρήγορη απο ποτέ!  </span>
+      <span className="AboutUsPage-text">
+        {" "}
+        Είμαστε μια ομάδα πέντε φοιτητών της σχολής Πληροφορικής Και
+        Τηλεπικοινωνιών που αποφασίσαμε να κάνουμε την επιλογή δραστηριοτήτων
+        για τα παιδιά πιο εύκολη και γρήγορη απο ποτέ!{" "}
+      </span>
 
-          <img classname="AboutUsPage-image" src={kidsPic} alt=""></img>
+      <img className="AboutUsPage-image" src={kidsPic} alt=""></img>
 
-          <span className="AboutUsPage-paragraph">- Τί κάνουμε? </span>
+      <span className="AboutUsPage-paragraph">- Τί κάνουμε? </span>
 
-          <span className="AboutUsPage-text"> Προσφέρουμε μια πλατφόρμα για εύκολη εξυπηρέτηση των εταιριών που παρέχουν δραστηριότητες για παιδιά και νέους με 
-          πλήρεις δυνατότητες αγοραπωλησίας εικoνικών εισητηρίων.</span>
+      <span className="AboutUsPage-text">
+        {" "}
+        Προσφέρουμε μια πλατφόρμα για εύκολη εξυπηρέτηση των εταιριών που
+        παρέχουν δραστηριότητες για παιδιά και νέους με πλήρεις δυνατότητες
+        αγοραπωλησίας εικoνικών εισητηρίων.
+      </span>
 
-          <img classname="AboutUsPage-image" src={internetPic} alt=""></img>
+      <img className="AboutUsPage-image" src={internetPic} alt=""></img>
 
-          <span className="AboutUsPage-paragraph">- Γιατί να χρησιμοποιήσετε τις υπηρεσίες μας? </span>
+      <span className="AboutUsPage-paragraph">
+        - Γιατί να χρησιμοποιήσετε τις υπηρεσίες μας?{" "}
+      </span>
 
-          <span className="AboutUsPage-text"> Γιατί με τις προηγμένες τεχνικές marketing, παίρνουμε το πιο ανταγωνιστικό ποσοστό απο τις αγορές των
-          εισητηρίων σας, μεγιστοποιόντας τα δικά σας κέρδη! </span>
-
-
+      <span className="AboutUsPage-text">
+        {" "}
+        Γιατί με τις προηγμένες τεχνικές marketing, παίρνουμε το πιο
+        ανταγωνιστικό ποσοστό απο τις αγορές των εισητηρίων σας, μεγιστοποιόντας
+        τα δικά σας κέρδη!{" "}
+      </span>
     </div>
   );
 };
